95e5e82c4a72616b516853553b3c7263d7f9ccde
GirishRed <71594099+GirishRed@users.noreply.github.com>
2020-11-21 22:41:18 +0530

    NewDemoGame

---
+++ Game.js
@@ -0,0 +1,37 @@
+import { question } from 'readline-sync';
+
+var userName = question('Please Enter Your Name to Play A Game : \n');
+
+console.log('Konnichiwa '+userName+'\n');
+console.log('Welcome to Naruto quiz \n');
+var score=0;
+
+function Game(ques,ans)
+{
+  var userAns = question(ques);
+
+  if(userAns.toUpperCase() === ans.toUpperCase())
+  {
+      console.log('Correct Answer..!');
+      score++;
+     
+  }
+  else if(userAns.toUpperCase()==='a'&&'b'&&'c'&&'d')
+  {
+    console.log('Wrong Answer :( ');
+
+  }
+  else{
+    console.log('Invalid input '+userName+' your score for this question is 0');
+  }
+   console.log('Your score is : '+score);
+   console.log('---------------------\n')
+}
+
+Game('1.What was Kakashi father name? \na.Minato\nb.Sakumo Hatake\nc.Madara\nd.Rock Leei\n','b');
+Game('2.What is Naruto fav food ?\n a.boiled egg\n b.Dumpling\n c.Ramen\n','c');
+Game('3.Girish fav character is Madara?\na.yes\nb.no\n','b');
+Game('4.Real name of tobi is\na.Kabuto\nb.Itachi\nc.Obito\n','c' );
+
+   console.log(userName+' Your final score is : '+score);
+
